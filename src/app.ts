927220126925483f572df1993db91c1b6a6e1eb0
Manohar-29 <manohar.dewoolkar97@gmail.com>
2024-07-28 22:53:49 +0530

    change redireted url

--- src/app.ts
+++ src/app.ts
@@ -6,14 +6,11 @@ import errorMiddleware from './middlewares/errorMiddleware';
 import cookieParser from 'cookie-parser';
 import "./config/passportJwtStratergy"
 import "./config/passwordGoogleStratergy"
-import googleRouter from './routes/google.routes';
-// import session from 'express-session';
+// import googleRouter from './routes/google.routes';
 // create app 
 const app: Application = express();
 
 // middleware
-
-
 const options = {
     origin: process.env.FRONTEND_URL,
     credentials: true,
@@ -21,27 +18,14 @@ const options = {
 };
 
 app.use(cors(options));
-
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
-
-
-// app.use(session({
-//     secret: process.env.SESSION_SECRET as string,
-//     resave: false,
-//     saveUninitialized: true,
-//     cookie: { secure: process.env.NODE_ENV === 'production' }
-// }));
-
 app.use(passport.initialize());
-// app.use(passport.session());
-
-
 
 //  routes
 app.use("/api/v1", router)
-app.use(googleRouter)
+// app.use(googleRouter)
 
 // common error handler
 app.use(errorMiddleware)
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import authRouter from "./auth.routes";
+import googleRouter from "./google.routes";
 
 const router: Router = Router();
 
 router.use('/auth', authRouter)
+router.use(googleRouter)
 
 
 export default router
